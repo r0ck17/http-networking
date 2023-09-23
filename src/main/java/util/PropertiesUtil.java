07179e67bd9589e21b0cdb92af9910b82c61e76c
Evgeniy <paut.evg@yandex.ru>
2023-09-23 22:21:00 +0300

    feature: add properties util class

---
+++ src/main/java/util/PropertiesUtil.java
@@ -0,0 +1,37 @@
+package util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private PropertiesUtil() {
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    public static int getInt(String key) {
+        String value = PROPERTIES.getProperty(key);
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("Invalid integer value for key: " + key, e);
+        }
+    }
+
+    private static void loadProperties() {
+        try (InputStream inputStream = PropertiesUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
